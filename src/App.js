728b613a6d8fe26f265aed2184b8c46971ab7a61
suthyscott <sutherlandscott21@gmail.com>
2023-02-20 10:41:46 -0700

    axios and APIs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios'
 
 function App() {
+  const [chars, setChars] = useState([])
+
+  const fetchChars = () => {
+    axios.get('https://swapi.dev/api/people')
+      .then((res) => { 
+        setChars(res.data)
+      })
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
