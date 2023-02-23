379e5ec1d4dd6e475ba1dce9ce6d9acf3e430774
suthyscott <sutherlandscott21@gmail.com>
2023-02-23 15:56:31 -0700

    adding and removing back into same index

--- src/App.js
+++ src/App.js
@@ -1,46 +1,59 @@
-import './App.css';
-import { useState, useEffect } from 'react';
-import axios from 'axios'
-import Mapper from './Mapper';
-import Favorites from './Favorites';
+import "./App.css"
+import { useState, useEffect } from "react"
+import axios from "axios"
+import Mapper from "./Mapper"
+import Favorites from "./Favorites"
 
 function App() {
-  const [chars, setChars] = useState([])
-  const [favorites, setFavorites] = useState([])
+    const [chars, setChars] = useState([])
+    const [favorites, setFavorites] = useState([])
 
-  const fetchChars = () => {
-    axios.get('https://swapi.dev/api/people')
-      .then((res) => { 
-        setChars(res.data.results)
-      })
-      .catch(err => console.log(err))
-  }
+    const fetchChars = () => {
+        axios
+            .get("https://swapi.dev/api/people")
+            .then(res => {
+                setChars(res.data.results)
+            })
+            .catch(err => console.log(err))
+    }
 
- useEffect(() => {
-  fetchChars()
- }, [])
+    useEffect(() => {
+        fetchChars()
+    }, [])
 
- const addNewFavorite = charObj => {
-  setFavorites([...favorites, charObj])
- }
+    const addNewFavorite = charObj => {
+        const index = chars.findIndex(char => charObj.name === char.name)
+        charObj.index = index
+        setFavorites([...favorites, charObj])
 
- const removeFromFavorites = name => {
-  const index = favorites.findIndex(fav => fav.name === name)
+        chars.splice(index, 1)
 
-  favorites.splice(index, 1)
+        setChars([...chars])
+    }
 
-  setFavorites([...favorites])
- }
+    const removeFromFavorites = name => {
+        const index = favorites.findIndex(fav => fav.name === name)
+        chars.splice(favorites[index].index, 0, favorites[index])
 
-// useEffect(fetchChars, [])
+        favorites.splice(index, 1)
 
-//  console.log(favorites)
-  return (
-    <div className="App">
-        <Mapper chars={chars} addNewFavorite={addNewFavorite}/>
-        <Favorites favorites={favorites} removeFromFavorites={removeFromFavorites}/>
-    </div>
-  );
+        setFavorites([...favorites])
+
+        setChars([...chars])
+    }
+
+    // useEffect(fetchChars, [])
+
+    //  console.log(favorites)
+    return (
+        <div className="App">
+            <Mapper chars={chars} addNewFavorite={addNewFavorite} />
+            <Favorites
+                favorites={favorites}
+                removeFromFavorites={removeFromFavorites}
+            />
+        </div>
+    )
 }
 
-export default App;
+export default App
